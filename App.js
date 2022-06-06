1112e44cbf483aac6fd72b6da096d8be9b313b96
Kaustavi <kaustavi@itobuz.com>
2022-06-06 10:33:11 +0530

    get data from firestore realtime data WIP

--- App.js
+++ App.js
@@ -7,7 +7,7 @@
  */
 
 import React, {useState, useEffect} from 'react';
-import MapView, {PROVIDER_GOOGLE, Marker} from 'react-native-maps';
+import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
 
  // import all the components we are going to use
 import {
@@ -17,48 +17,26 @@ import {
   StyleSheet,
   Image,
   Button,
+  Platform
 } from 'react-native';
 
  //import all the components we are going to use.
  import Geolocation from '@react-native-community/geolocation';
 import {firebaseRef, geoFire} from './firebase';
+import DeviceInfo from 'react-native-device-info';
+import {ref, onValue} from 'firebase/database';
  
  const App = () => {
-   const [currentLongitude, setCurrentLongitude] = useState(0);
-   const [currentLatitude, setCurrentLatitude] = useState(0);
-   const [locationStatus, setLocationStatus] = useState('');
+   const [coordinate, setCoordinate] = useState({latitude: 0, longitude: 0});
 
-  const initialRegion = {
-    latitude: Number(currentLatitude),
-    longitude: Number(currentLongitude),
-    latitudeDelta: 0.0922,
-    longitudeDelta: 0.0421,
-  };
- 
-   const getOneTimeLocation = () => {
-     setLocationStatus('Getting Location ...');
-     Geolocation.getCurrentPosition(
-      position => {
-        setLocationStatus('You are Here');
-        let currentLongitude = JSON.stringify(position.coords.longitude);
-        let currentLatitude = JSON.stringify(position.coords.latitude);
-        setCurrentLongitude(currentLongitude);
-        setCurrentLatitude(currentLatitude);
-      },
-      error => {
-        console.log({error});
-        setLocationStatus(error.message);
-      },
-      {enableHighAccuracy: true, timeout: 30000, maximumAge: 1000},
-    );
-   };
+   const deviceInfo = DeviceInfo.getDeviceId();
 
-
-   function storedData() {
-    console.log('clicked');
-    geoFire
-      .set('my_Location', [Number(currentLatitude), Number(currentLongitude)])
-      .then(
+  function storedData() {
+    var geoQuery = geoFire.query({
+      center: [coordinate.latitude, coordinate.longitude],
+      radius: 10.5,
+    });
+    geoFire.set(deviceInfo, geoQuery.center()).then(
         function () {
           console.log('Provided key has been added to GeoFire');
         },
@@ -68,36 +46,60 @@ import {firebaseRef, geoFire} from './firebase';
      );
     
    }
- 
+
+   function getStoredData() {
+    const locationRef = ref(
+      firebaseRef,
+      'geoLocation/-N3cvLhMPuyCYC0N4MgJ/k69v1_64',
+    );
+    onValue(locationRef, snapshot => {
+      console.log('stored data: ' + snapshot.val());
+    });
+      
+   }
+
+  const getDynamicLocation = () => {
+    Geolocation.watchPosition(
+      position => {
+        const newCoordinate = {
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+        };
+        console.log(
+          'my coordinate: ' + (newCoordinate.latitude, newCoordinate.longitude),
+        );
+        setCoordinate(newCoordinate);
+      },
+      {enableHighAccuracy: true, timeout: 30000, maximumAge: 1000},
+    );
+    storedData();
+  };
+
    useEffect(() => {
-    getOneTimeLocation();
-    firebaseRef.get('my_Location').then(function (location) {
-        const locVal = location.val();
-        console.log(locVal);
-      });
-   }, []);
+    if (Platform.OS === 'ios') {
+      getStoredData();
+    } else {
+      getDynamicLocation();
+    }
+   });
    return (
     <SafeAreaView style={{flex: 1}}>
        <View style={styles.container}>
-        {currentLatitude !== (null || undefined) &&
-          currentLongitude !== (null || undefined) && (
+        {coordinate !== (null || undefined) && (
           <MapView
               provider={PROVIDER_GOOGLE}
               style={{height: 300, width: '100%'}}
-              region={initialRegion}
+              region={{
+                latitude: Number(coordinate.latitude),
+                longitude: Number(coordinate.longitude),
+                latitudeDelta: 0.0922,
+                longitudeDelta: 0.0421,
+              }}
               onUserLocationChange={v => {
-                setCurrentLongitude(v.nativeEvent.coordinate.longitude);
-                setCurrentLatitude(v.nativeEvent.coordinate.latitude);
+              setCoordinate(v.nativeEvent.coordinate);
               }}
               showsUserLocation>
-            <MapView.Marker
-                coordinate={{
-                  latitude: Number(currentLatitude),
-                  longitude: Number(currentLongitude),
-                }}
-                title="MyCoordinate"
-              />
-
+            <MapView.Marker coordinate={coordinate} title="MyCoordinate" />
           </MapView>
         )}
         <View style={styles.container}>
@@ -107,23 +109,9 @@ import {firebaseRef, geoFire} from './firebase';
              }}
             style={{width: 100, height: 100}}
            />
-           <Text style={styles.boldText}>{locationStatus}</Text>
-           <Text
-             style={{
-               justifyContent: 'center',
-               alignItems: 'center',
-               marginTop: 16,
-             }}>
-             Longitude: {currentLongitude}
-           </Text>
-           <Text
-             style={{
-              justifyContent: 'center',
-              alignItems: 'center',
-              marginTop: 16,
-            }}>
-            Latitude: {currentLatitude}
-           </Text>
+           <Text style={styles.boldText}>Device coordinate</Text>
+          <Text style={styles.boldText}>Longitude: {coordinate.longitude}</Text>
+          <Text style={styles.boldText}>Latitude: {coordinate.latitude}</Text>
           <View style={{marginTop: 20}}>
             <Button title="Store to database" onPress={() => storedData()} />
            </View>
